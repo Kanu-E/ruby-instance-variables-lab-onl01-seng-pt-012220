28d9ef4147f4957a6e0629905614f69c91674068
Chukwuemeka Kanu <chukwuemekakanu@gmail.com>
2020-02-18 19:26:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,5 @@
 class Dog
+<<<<<<< HEAD
  
   def name=(dogs_name)
     @this_dogs_name = dogs_name
@@ -13,3 +14,16 @@ lassie = Dog.new
 lassie.name = "Lassie"
 
 puts lassie.name
+=======
+  def name=(dog_name)
+    this_dogs_name = dog_name
+  end
+ 
+  def name
+    this_dogs_name
+  end
+end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+>>>>>>> a6e15914dd9165456be3eadfb2b84474fd7ff3a5
